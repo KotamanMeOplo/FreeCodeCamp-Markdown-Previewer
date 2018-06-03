7d62c5cfa5d79975cbe246a17adf1328a8b529f5
Andreas <AndrewNaTesla@gmail.com>
2018-06-03 16:45:20 +0300

    Removed all css files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import Editor from './Components/Editor.js';
 import Preview from './Components/Preview.js';
-import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  font-size: 16px;
+  justify-content: space-evenly;
+  flex-flow: row wrap;
+  align-content: center;
+`;
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrapper className="App">
         <Editor />
         <Preview />
-      </div>
+      </Wrapper>
     );
   }
 }
